5e09683efc248e50217e95458f75761133547bc9
Veronika <veronika.slesarchyk@gmail.com>
2025-11-30 17:54:42 +0300

    Revert "homework 18,19,20"
    This reverts commit 53295ec6e09466d081a5d7027d1728c60d1a2f88.

--- src/by/homework/task19/Continent.java
+++
@@ -1,5 +0,0 @@
-package by.homework.task19;
-
-public enum Continent {
-    EUROPE, ASIA, SOUTH_AMERICA, AFRICA, NORTH_AMERICA, AUSTRALIA
-}
--- src/by/homework/task19/StringProcessor.java
+++
@@ -1,6 +0,0 @@
-package by.homework.task19;
-
-//1 Создать интерфейс, который будет обрабатывать входящий параметр типа String.
-public interface StringProcessor {
-    String process(String s); //метод
-}
